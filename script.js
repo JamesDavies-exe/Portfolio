2dfa5a90bc4a549c886c99328ab2a3220ec9a434
James Davies <daviesjamesemlyn@gmail.com>
2022-12-09 19:11:14 +0100

    darkmode

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ new simpleParallax(image, {
 	scale: 1.5,
 	orientation: 'right'
 });
+
+
+	var hour = (new Date).getHours();
+  
+	if (hour >= 19) {
+		document.getElementById('main').removeClass('light').addClass('dark');
+	} else {
+		document.getElementById('main').removeClass('dark').addClass('light');
+	}
+	console.log(hour);
+  
