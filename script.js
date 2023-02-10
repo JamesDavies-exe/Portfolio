6832612900e5399d3a12fb52fecdce6e30271344
James Davies <daviesjamesemlyn@gmail.com>
2023-02-10 17:13:50 +0100

    86

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-AOS.init();
-
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image, {
-	delay: .6,
-	scale: 1.5,
-	orientation: 'right'
-});
-
 function darkMode() {
 	var element = document.getElementById("main");
 	element.classList.toggle("dark");
